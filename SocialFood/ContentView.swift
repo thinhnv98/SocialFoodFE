fbc493bf577ac97ec1233cb6e6ec1d12b1fbdf9f
Thinh.Nguyen <nguyenvanthinhab3@gmail.com>
2021-06-18 12:02:21 +0700

    Initial Commit

---
+++ SocialFood/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SocialFood
+//
+//  Created by Thịnh Nguyễn on 18/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SocialFood/SocialFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialFoodApp.swift
+//  SocialFood
+//
+//  Created by Thịnh Nguyễn on 18/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SocialFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
