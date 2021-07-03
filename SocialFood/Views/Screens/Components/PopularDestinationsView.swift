6ad7c7b6fa502e7ec949b804736d03a7eb97f300
chutientrong <42104378+chutientrong@users.noreply.github.com>
2021-07-03 20:01:58 +0700

    trong-final

--- SocialFood/Views/Screens/Components/PopularDestinationsView.swift
+++ SocialFood/Views/Screens/Components/PopularDestinationsView.swift
@@ -31,7 +31,11 @@ struct PopularDestinationsView: View{
                 HStack(spacing: 8.0){
                     ForEach(destinations, id: \.self) { destination in
                         NavigationLink(
-                            destination: NavigationLazyView(PopularDestinationDetailsView(destination: destination)))
+                            destination: NavigationLazyView(PopularDestinationDetailsView(destination: destination)),
+                            label: {
+                                PopularDestinationTile(destination: destination)
+                                .padding(.bottom)
+                            })
                     }
                 }.padding(.horizontal)
             }
@@ -131,20 +135,107 @@ struct PopularDestinationDetailsView: View {
                         .font(.system(size: 14))
                     Spacer()
                 }
+//
 //                HStack{Spacer()}
             }
             .padding(.horizontal)
             
+            HStack {
+                Text("Location")
+                    .font(.system(size: 18, weight: .semibold))
+                Spacer()
+                
+                Button(action: {
+                    isShowingAttractions.toggle()
+                }, label: {
+                    Text("\(isShowingAttractions ? "Hide" : "Show") Attractions")
+                        .font(.system(size: 14, weight: .semibold))
+                })
+                
+                Toggle("Title title", isOn: $isShowingAttractions)
+                    .labelsHidden()
+            }.padding(.horizontal)
             
+            Map(coordinateRegion: $region, annotationItems: isShowingAttractions ? attractions : []) { attraction in
+                MapAnnotation(coordinate: .init(latitude: attraction.latitude, longitude: attraction.longitute)) {
+                    CustomeMapAnnotation(attraction: attraction)
+                }
+            }
+            .frame(height: 300)
             
         }.navigationBarTitle(destination.name, displayMode: .inline)
     }
     
-    
+    let attractions: [Attraction] = [
+        .init(name: "Eiffel Tower", imageName: "1.eiffel", latitude: 48.858605, longitute: 2.2946),
+        .init(name: "Champs-Elysees", imageName: "1.newyork", latitude: 48.866867, longitute: 2.311780),
+        .init(name: "Louvre Museum", imageName: "1.museum", latitude: 48.860288, longitute: 2.337789)
+    ]
 }
 
+struct CustomeMapAnnotation: View {
+    let attraction: Attraction
+    
+    var body: some View {
+        VStack{
+            Image(attraction.imageName)
+                .resizable()
+                .frame(width: 80, height: 60)
+                .cornerRadius(4)
+                .overlay(RoundedRectangle(cornerRadius: 4)
+                            .stroke(Color(.init(white: 0, alpha: 0.5)))
+                
+                )
+            
+            Text(attraction.name)
+                .font(.system(size: 12, weight: .semibold))
+                .padding(.horizontal, 6)
+                .padding(.vertical, 4)
+                .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing))
+                .foregroundColor(.white)
+                .overlay(RoundedRectangle(cornerRadius: 4)
+                            .stroke(Color(.init(white: 0, alpha: 0.5)))
+                
+                )
+        }.shadow(radius: 5)
+    }
+}
 
+struct Attraction: Identifiable {
+    let id = UUID().uuidString
+    
+    let name, imageName: String
+    let latitude, longitute: Double
+}
 
+struct PopularDestinationTile: View {
+    
+    let destination: Destination
+    
+    var body: some View{
+        VStack(alignment: .leading, spacing: 0){
+            Image(destination.imageName)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 125, height: 125)
+                .cornerRadius(4)
+                .padding(.horizontal, 6)
+                .padding(.vertical, 6)
+            
+            Text(destination.name)
+                .font(.system(size: 12, weight: .semibold))
+                .padding(.horizontal, 12)
+                .foregroundColor(Color(.label))
+            
+            Text(destination.country)
+                .font(.system(size: 12, weight: .semibold))
+                .padding(.horizontal, 12)
+                .padding(.bottom, 8)
+                .foregroundColor(.gray)
+        }
+        .asTile()
+    }
+}
 
 struct PopularDestinationsView_Previews: PreviewProvider {
     static var previews: some View {
